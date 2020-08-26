3c8fc5a77a58b8c5f887b44199c7f012c40c33ad
Nikolina <nikolina.okun@gmail.comgit config --global user.name Nikolina-Niky>
2020-08-26 19:33:50 +0200

    changing url for image

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component  {
 	};
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3001/signin', {
+		fetch('https://polar-escarpment-85300.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
